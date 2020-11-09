9211a2f391abfedcc296e58c47e8249b959d0c6d
王峰 <wangfeng_dq@piesat.cn>
2020-11-10 04:22:29 +0800

    1

---
+++ worley_noise/perlin.h
@@ -0,0 +1,188 @@
+// perlin.cpp : Defines the entry point for the console application.
+//
+#include <math.h>
+#include <stdio.h>
+
+
+float random(float x, float y) {
+	float dotval = x * 12.9898  + y * 78.2333 ;
+	float sinval = abs( sin(dotval) ) ;
+	float temp1 = sinval * 43758.54f ;
+	return temp1 - (int)temp1;
+}
+float random3d(float x, float y,float z) {
+	float dotval = x * 112.9898 + y * 178.2333 + z* 142.827 ;
+	float sinval = abs(sin(dotval));
+	float temp1 = sinval * 43758.54f;
+	return temp1 - (int)temp1;
+}
+
+
+float mix(float vmin, float vmax, float x)
+{
+	float u = x*x*(3.0 - 2.0*x);
+	return vmin + (vmax - vmin) * u;
+}
+
+float noise( float x , float y ) {
+	int ix = floor(x);
+	int iy = floor(y);
+
+	float fx = x - ix;
+	float fy = y - iy;
+ 
+	// Four corners in 2D of a tile
+	float a = random(ix,iy);
+	float b = random(ix + 1 , iy );
+	float c = random(ix , iy + 1 );
+	float d = random(ix + 1 , iy + 1 );
+
+	float ux = fx * fx * (3.f - 2.f * fx);
+	float uy = fy * fy * (3.f - 2.f * fy);
+
+	float inter0 = mix(a, b, fx);
+	float inter1 = mix(c, d, fx);
+	float inter2 = mix(inter0, inter1, fy);
+
+	return inter2;
+}
+
+float noise3d(float x, float y,float z) {
+	int ix = floor(x);
+	int iy = floor(y);
+	int iz = floor(z);
+	float fx = x - ix;
+	float fy = y - iy;
+	float fz = z - iz;
+
+	// Four corners in 2D of a tile
+	float a = random3d(ix      , iy     , iz     );
+	float b = random3d(ix + 1  , iy     , iz     );
+	float c = random3d(ix      , iy + 1 , iz     );
+	float d = random3d(ix + 1  , iy + 1 , iz     );
+
+	float a1 = random3d(ix, iy, iz+1);
+	float b1 = random3d(ix + 1, iy, iz+1);
+	float c1 = random3d(ix, iy + 1, iz+1);
+	float d1 = random3d(ix + 1, iy + 1, iz+1);
+
+	//float ux = fx * fx * (3.f - 2.f * fx);
+	//float uy = fy * fy * (3.f - 2.f * fy);
+	//float uz = fz * fz * (3.f - 2.f * fz);
+
+	float inter0 = mix(a, b, fx);
+	float inter1 = mix(c, d, fx);
+	float inter2 = mix(inter0, inter1, fy);
+
+	float inter00 = mix(a1, b1, fx);
+	float inter11 = mix(c1, d1, fx);
+	float inter22 = mix(inter00, inter11, fy);
+
+	float inter33 = mix(inter2, inter22, fz);
+
+	return inter33;
+}
+
+float fbm(float ptx , float pty ) {
+	// Initial values
+	float value = 0.0;
+	float amplitude = .5;
+	float frequency = 0.;
+	//
+	// Loop of octaves
+	int oct = 6;
+
+	for (int i = 0; i < oct ; i++) {
+		value += amplitude * noise( ptx , pty );
+		ptx *= 2.f;
+		pty *= 2.f;
+		amplitude *= .5f;
+	}
+	return value;
+}
+
+float fbm3d(float ptx, float pty ,float ptz) {
+	// Initial values
+	float value = 0.0;
+	float amplitude = .5;
+	float frequency = 0.;
+	//
+	// Loop of octaves
+	int oct = 6 ;//use 6
+
+	for (int i = 0; i < oct; i++) {
+		value += amplitude * noise3d(ptx, pty,ptz);
+		ptx *= 2.f;
+		pty *= 2.f;
+		ptz *= 2.f;
+		amplitude *= .5f;
+	}
+	return value;
+}
+
+int perlin()
+{
+	char outfile[] = "perlin256";
+
+	int xsize = 256;
+	int ysize = 256;
+	int zsize = 256;
+	int xysize = 256 * 256;
+	int asize = 256 * 256 * 256;
+
+	float scale = 4.f;
+
+	float* buffer = new float[256 * 256*256];
+	unsigned char* buffer1 = new unsigned char[256 * 256 * 256];
+
+	FILE* pf = fopen(outfile, "wb");
+
+	float valmin = 9999.f;
+	float valmax = -9999.f;
+
+	for (int iz = 0; iz < 256; ++iz)
+	{
+		int it0 = iz * 256 * 256;
+		for (int iy = 0; iy < 256; ++iy)
+		{
+			for (int ix = 0; ix < 256; ++ix)
+			{
+				float ptx = ix * scale / 256.f  ;
+				float pty = iy * scale / 256.f  ;
+				float ptz = iz * scale / 256.f  ;
+
+				float color1 = fbm3d(ptx, pty,ptz);//float color1 = fbm(ptx, pty);
+				if (color1 > valmax)
+				{
+					valmax = color1;
+				}
+				if (color1 < valmin)
+				{
+					valmin = color1;
+				}
+				buffer[it0 + iy * 256 + ix] = color1;
+			}
+		}
+		printf("band:%d.", iz);
+		//if (iz == 0) break;
+	}
+
+	float dist = valmax - valmin;
+
+	for (int it = 0; it < asize; ++it)
+	{
+		unsigned char color2 = (buffer[it] - valmin) / dist * 255;
+		buffer1[it] = color2;
+	}
+
+
+	fwrite(buffer1, 256 * 256 * 256, 1, pf);
+	fclose(pf);
+
+	delete[] buffer;
+	delete[] buffer1;
+
+
+
+	return 0;
+}
---
+++ worley_noise/worley_noise.cpp
@@ -0,0 +1,381 @@
+// worley_noise.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <string>
+#include "perlin.h"
+#include <vector>
+using namespace std;
+
+double wrandom()
+{
+	return (double)rand() / (double)RAND_MAX;
+}
+
+//sx,sy,sz seed in grid(sx,sy,sz) 
+//dxArr dx
+//px,py,pz pixel coordinates of the seed in grid(sx,sy,sz)
+
+void getSeedPixelCoors(const int sx, const int sy, const int sz, int gridsize,int gridxnum,int gridynum,int gridznum,
+	int* dxArr,int* dyArr,int* dzArr,
+	int& px, int& py, int& pz)
+{
+	int sx2 = sx; 
+	int x0 = sx*gridsize;
+	if (sx2 < 0){ sx2 = gridxnum - 1; x0 = -gridsize; }
+	else if (sx2 == gridxnum ){ sx2 = 0; x0 = gridsize * gridxnum; }
+
+	int sy2 = sy;
+	int y0 = sy*gridsize;
+	if (sy2 < 0){
+		sy2 = gridynum - 1; y0 = -gridsize;
+	}
+	else if (sy2 == gridynum ) {
+		sy2 = 0;
+		y0 = gridsize*gridynum;
+	}
+
+	int sz2 = sz;
+	int z0 = sz*gridsize;
+	if (sz2 < 0){
+		sz2 = gridznum - 1;
+		z0 = -gridsize;
+	}
+	else if (sz2 == gridznum ){
+		sz2 = 0;
+		z0 = gridsize*gridznum;
+	}
+
+	int igrid = sz2*gridxnum*gridynum + sy2*gridxnum + sx2;
+	px = x0 + dxArr[igrid];
+	py = y0 + dyArr[igrid];
+	pz = z0 + dzArr[igrid];
+
+}
+
+void getSeedPixelCoors2(const int sx, const int sy, const int sz,
+	int gridsize,int gridxnum,int gridynum,int gridznum,
+	vector<int>& dxArr,vector<int>& dyArr,vector<int>& dzArr,
+	int& px, int& py, int& pz)
+{
+	int sx2 = sx; 
+	int x0 = sx*gridsize;
+	if (sx2 < 0){ sx2 = gridxnum - 1; x0 = -gridsize; }
+	else if (sx2 == gridxnum ){ sx2 = 0; x0 = gridsize * gridxnum; }
+
+	int sy2 = sy;
+	int y0 = sy*gridsize;
+	if (sy2 < 0){
+		sy2 = gridynum - 1; y0 = -gridsize;
+	}
+	else if (sy2 == gridynum ) {
+		sy2 = 0;
+		y0 = gridsize*gridynum;
+	}
+
+	int sz2 = sz;
+	int z0 = sz*gridsize;
+	if (sz2 < 0){
+		sz2 = gridznum - 1;
+		z0 = -gridsize;
+	}
+	else if (sz2 == gridznum ){
+		sz2 = 0;
+		z0 = gridsize*gridznum;
+	}
+
+	int igrid = sz2*gridxnum*gridynum + sy2*gridxnum + sx2;
+	px = x0 + dxArr[igrid];
+	py = y0 + dyArr[igrid];
+	pz = z0 + dzArr[igrid];
+
+}
+
+
+void worley(int width,int height,int depth,int gridSize,int randseed, vector<float>& retdata)
+{
+	srand(randseed);
+
+	int xGridnum = width / gridSize  ;
+	int yGridnum = height / gridSize  ;
+	int zGridnum = depth / gridSize   ;
+
+	retdata.resize( (size_t)width*height*depth ) ;
+	
+	cout << "gridnum:" << xGridnum << " " << yGridnum << " " << zGridnum << endl;
+
+	vector<int> dxArr(xGridnum*yGridnum*zGridnum);
+	vector<int> dyArr(xGridnum*yGridnum*zGridnum);
+	vector<int> dzArr(xGridnum*yGridnum*zGridnum);
+
+	cout << "begin random seeding." << endl;
+
+	int itgrid = 0;
+	for (int izg = 0; izg < zGridnum; ++izg)
+	{
+		for (int iyg = 0; iyg < yGridnum; ++iyg)
+		{
+			for (int ixg = 0; ixg < xGridnum; ++ixg)
+			{
+				float rx = wrandom();
+				float ry = wrandom();
+				float rz = wrandom();
+
+				int rdx = rx * gridSize;
+				int rdy = ry*gridSize;
+				int rdz = rz*gridSize;
+
+				int xcoor =   rdx;
+				int ycoor =  rdy;
+				int zcoor =   rdz;
+
+				dxArr[itgrid] = xcoor;
+				dyArr[itgrid] = ycoor;
+				dzArr[itgrid] = zcoor;
+				itgrid++;
+			}
+		}
+	}
+	cout << "seed is ok. begin fill buffer ..." << endl;
+	float theMaxValue = 0;
+	float theMinValue = 99999;
+	int it = 0;
+	for (int iz = 0; iz < depth; ++iz)
+	{
+		for (int iy = 0; iy < height; ++iy)
+		{
+			for (int ix = 0; ix < width; ++ix)
+			{
+				int xg = ix / gridSize;
+				int yg = iy / gridSize;
+				int zg = iz / gridSize;
+
+				float dist2 = 99999;
+				for (int zz = zg-1; zz <= zg+1 ; ++zz)
+				{
+					for (int yy = yg - 1; yy <= yg + 1; ++yy)
+					{
+						for (int xx = xg - 1; xx <= xg + 1; ++xx)
+						{
+							int seedx = 0;
+							int seedy = 0;
+							int seedz = 0;
+							getSeedPixelCoors2(xx, yy, zz, gridSize,
+								xGridnum, yGridnum, zGridnum,
+								dxArr, dyArr, dzArr,
+								seedx, seedy, seedz);
+ 
+							float tdist2 = (seedx - ix)*(seedx - ix)
+								+ (seedy - iy)*(seedy - iy)
+								+ (seedz - iz)*(seedz - iz);
+							tdist2 = sqrtf(tdist2);
+							if (tdist2 < dist2)
+							{
+								dist2 = tdist2;
+							}
+						}
+					}
+				}
+				if (dist2 > theMaxValue) theMaxValue = dist2;
+				if (dist2 < theMinValue) theMinValue = dist2;
+ 
+				retdata[iz*width*height + iy*width + ix] =  dist2 ;
+			}
+		}
+		if( iz % 8==0 )cout << iz << " " ;
+	}
+	cout<<"min:"<<theMinValue<<",max:"<<theMaxValue<<endl;
+	float diff = theMaxValue - theMinValue;
+	for(int it = 0 ; it < retdata.size();++it )
+	{
+		float tval = (retdata[it] - theMinValue)/diff;
+		retdata[it] = tval;
+	}
+}
+
+
+
+
+int main(int argc, char* argv[])
+{
+	cout << "A program to make 3D worley noise. wangfengdev@163.com 2019-10-14." << endl;
+	cout << "call:worley_noise width height depth gridsize outfile seed [invert]" << endl;
+
+	//perlin
+	//perlin() ; // i think a scale of 4 perlin is ok.2020-11-10
+
+	int width = 256;
+	int height = 256;
+	int depth = 256;
+	int gridSize = 32;
+	char outfile[512] = "outfile";
+	int randseed = 0;
+	int invert = 1;
+	//if (argc == 7 || argc==8 )
+	//{
+	//	width = atof(argv[1]);
+	//	height = atof(argv[2]);
+	//	depth = atof(argv[3]);
+	//	gridSize = atof(argv[4]);
+	//	strcpy(outfile , argv[5] ) ;
+	//	randseed = atof(argv[6]);
+	//	if (argc == 8)
+	//	{
+	//		invert = atof(argv[7]);
+	//	}
+	//}
+	//else
+	//{
+	//	cout << "error: no enough params." << endl;
+	//	return 11;
+	//}
+
+	cout << "width height depth :" << width << " " << height << " " << depth << endl;
+	cout << "gridsize:" << gridSize << endl;
+	cout << "outfile:" << outfile << endl;
+	cout << "rand seed:" << randseed << endl;
+
+	srand(randseed);
+
+	int xGridnum = width / gridSize  ;
+	int yGridnum = height / gridSize  ;
+	int zGridnum = depth / gridSize   ;
+
+	int * buffer = new int[width*height*depth];
+
+	cout << "gridnum:" << xGridnum << " " << yGridnum << " " << zGridnum << endl;
+
+	int* dxArr = new int[xGridnum*yGridnum*zGridnum];
+	int* dyArr = new int[xGridnum*yGridnum*zGridnum];
+	int* dzArr = new int[xGridnum*yGridnum*zGridnum];
+
+	cout << "begin random seeding." << endl;
+
+	int itgrid = 0;
+	for (int izg = 0; izg < zGridnum; ++izg)
+	{
+		for (int iyg = 0; iyg < yGridnum; ++iyg)
+		{
+			for (int ixg = 0; ixg < xGridnum; ++ixg)
+			{
+				float rx = wrandom();
+				float ry = wrandom();
+				float rz = wrandom();
+
+				int rdx = rx * gridSize;
+				int rdy = ry*gridSize;
+				int rdz = rz*gridSize;
+
+				int xcoor =   rdx;
+				int ycoor =  rdy;
+				int zcoor =   rdz;
+
+				dxArr[itgrid] = xcoor;
+				dyArr[itgrid] = ycoor;
+				dzArr[itgrid] = zcoor;
+				itgrid++;
+			}
+		}
+	}
+
+
+	cout << "seed is ok. begin fill buffer ..." << endl;
+	int theMaxValue = 0;
+	int theMinValue = 99999;
+	int it = 0;
+	for (int iz = 0; iz < depth; ++iz)
+	{
+		for (int iy = 0; iy < height; ++iy)
+		{
+			for (int ix = 0; ix < width; ++ix)
+			{
+				int xg = ix / gridSize;
+				int yg = iy / gridSize;
+				int zg = iz / gridSize;
+
+				int dist2 = 99999;
+				for (int zz = zg-1; zz <= zg+1 ; ++zz)
+				{
+					for (int yy = yg - 1; yy <= yg + 1; ++yy)
+					{
+						for (int xx = xg - 1; xx <= xg + 1; ++xx)
+						{
+							int seedx = 0;
+							int seedy = 0;
+							int seedz = 0;
+							getSeedPixelCoors(xx, yy, zz, gridSize,
+								xGridnum, yGridnum, zGridnum,
+								dxArr, dyArr, dzArr,
+								seedx, seedy, seedz);
+ 
+							int tdist2 = (seedx - ix)*(seedx - ix)
+								+ (seedy - iy)*(seedy - iy)
+								+ (seedz - iz)*(seedz - iz);
+							tdist2 = sqrtf(tdist2);
+							if (tdist2 < dist2)
+							{
+								dist2 = tdist2;
+
+							}
+						}
+					}
+				}
+				if (dist2 > theMaxValue) theMaxValue = dist2;
+				if (dist2 < theMinValue) theMinValue = dist2;
+ 
+				buffer[iz*width*height + iy*width + ix] =  dist2 ;
+			}
+		}
+		cout << iz << " " ;
+	}
+
+	unsigned char* outbuffer = new unsigned char[width*height*depth];
+	int asize = width*height*depth;
+	for (int it = 0; it < asize; ++it)
+	{
+		outbuffer[it] = 255.0*(buffer[it] - theMinValue*1.0) / (theMaxValue - theMinValue);
+		if (invert > 0)
+		{
+			outbuffer[it] = 255 - outbuffer[it];
+		}
+		
+	}
+
+	FILE* pf = fopen(outfile, "wb");
+	fwrite(outbuffer, width*height*depth, 1, pf);
+	fclose(pf);
+
+	{//write hdr file
+		string hdrfile = string(outfile) + string(".hdr");
+		FILE* pf2 = fopen(hdrfile.c_str(), "w");
+		fprintf(pf2, "ENVI\n");
+		fprintf(pf2, "description = {\n");
+		fprintf(pf2, "worley3d }\n");
+		fprintf(pf2, "samples = %d\n",width);
+		fprintf(pf2, "lines = %d\n",height);
+		fprintf(pf2, "bands = %d\n",depth);
+		fprintf(pf2, "header offset = 0\n");
+		fprintf(pf2, "file type = ENVI Standard\n");
+		fprintf(pf2, "data type = 1\n");
+		fprintf(pf2, "interleave = bsq\n");
+		fprintf(pf2, "sensor type = Unknown\n");
+		fprintf(pf2, "byte order = 0\n");
+		fprintf(pf2, "wavelength units = Unknown\n");
+		fclose(pf2);
+	}
+
+
+
+	delete[] outbuffer;
+	delete[] buffer;
+	delete[] dxArr;
+	delete[] dyArr;
+	delete[] dzArr;
+
+	return 0;
+}
+
